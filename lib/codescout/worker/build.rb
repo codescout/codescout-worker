d8845b7b2abff6756ad8e61355d1f0a90244d7e6
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-09-04 18:36:47 -0500

    Missing file

---
+++ lib/codescout/worker/build.rb
@@ -0,0 +1,46 @@
+require "docker"
+require "securerandom"
+
+module Codescout
+  class Worker
+    class Build
+      def initialize(push_token)
+        @push_token = push_token
+      end
+
+      def run
+        container = create_container
+
+        container.tap(&:start).attach do |stream, chunk|
+          STDOUT.puts("#{chunk}")
+        end
+
+        container.wait(120)
+        container.kill
+        container.delete(force: true)
+      end
+
+      private
+
+      def create_container
+        Docker::Container.create(
+          "name"         => "codescout-#{SecureRandom.hex(8)}",
+          "Cmd"          => ["codescout-runner"],
+          "Image"        => image_name,
+          "Env"          => env_vars,
+        )
+      end
+
+      def image_name
+        ENV["DOCKER_IMAGE"] || "codescout"
+      end
+
+      def env_vars
+        [
+          "CODESCOUT_URL=#{ENV["CODESCOUT_URL"]}",
+          "CODESCOUT_PUSH=#{@push_token}"
+        ]
+      end
+    end
+  end
+end
