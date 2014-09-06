2c3798ba331ef7eaba13b2cd132f4154586bd95a
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-09-06 14:50:49 -0500

    Add sidekiq configuration

--- init.rb
+++ init.rb
@@ -15,3 +15,11 @@ require_var("REDIS_URL")
 require_var("CODESCOUT_URL")
 require_var("DOCKER_HOST")
 require_var("DOCKER_IMAGE")
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV["REDIS_URL"] }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV["REDIS_URL"] }
+end
